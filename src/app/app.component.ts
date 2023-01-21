1be714290f1713197919507e0d05199f92babd99
HenryAprendo <hjsmipuc@gmail.com>
2023-01-21 15:06:36 -0500

    Cambio a formularios reactivo, y configuración del tamaño para laptop y tablet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'to-do-list';
 
-  task:string = '';
+  task = new FormControl('',[Validators.required, Validators.minLength(28)]);
 
+  showTask:string = '';
 
+  ngOnInit(){
+    this.task.valueChanges.subscribe( value => {
+      value ? this.showTask = value : this.showTask = '';
+    });
+  }
 
+  addTask(){
+    if(this.task.valid){
+      // codigo
+    }
+    else {
+      this.task.markAllAsTouched;
+    }
+  }
+
+  get fieldValid(){
+    return this.task.valid && this.task.touched;
+  }
+
+  get fieldInvalid(){
+    return this.task.invalid && this.task.touched;
+  }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
